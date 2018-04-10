5be46b2d1f2953ac9d6cdd8c678d580a59728343
Sunshine <guang334419520@126.com>
2018-04-10 17:45:44 +0800

    version 0.12

---
+++ client/client.cpp
@@ -0,0 +1,217 @@
+/**
+ * @Author: YangGuang <sunshine>
+ * @Date:   2018-04-10T15:19:20+08:00
+ * @Email:  guang334419520@126.com
+ * @Filename: client.cpp
+ * @Last modified by:   sunshine
+ * @Last modified time: 2018-04-10T17:26:58+08:00
+ */
+
+ #ifdef _WIN32_PLATFROM_
+  #include <winsock2.h>
+  #pragma comment(lib,"ws2_32.lib")
+  using Socket = SOCKET;
+ #else
+ #include <netinet/in.h>
+ #include <sys/socket.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <netinet/in.h>
+ #include <unistd.h>
+ #include <arpa/inet.h>
+ using Socket = int;
+ #endif
+
+ #include <iostream>
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <cstring>
+ #include <time.h>
+ #include <vector>
+ #include <string>
+ #include <map>
+ #include <fstream>
+
+ #define max(a, b) { (a) > (b) ? (a) : (b)}
+
+ const int KMaxLen = 4096;           // 消息Max
+ const int KServPort = 9999;         // server 端口号
+ const int LISTENQ = 20;             // listenq
+ const int KUserNameMax = 12;        // max len of the username
+ const int KUserPassMax = 16;        // max len of the password
+ const int KGroupName = 12;          // max len of the group
+ const int KDataMax = 2048;          // max len of the data
+ const char* KServAddr = "127.0.0.1";
+
+ /* 消息的类型 */
+ enum MessageFlags {
+   LeftBorder = 0,
+   PrivateChat,
+   AddFriend,
+   DelFriend,
+   PublicChat,
+   FindNear,
+   AgreeOrRefused,
+   Quit,
+   SigOut,
+   RightBorder,
+   SigIn = 64,
+   Register,
+   Succees = 127,
+   AccountOrPassError,
+   AccountError,
+   AgainErro
+ };
+ // 用于 好友同意，拒绝，或者登录成功或者失败的消息结构体
+ struct Flags {
+   MessageFlags flags;
+ };
+
+ /* 注册账号 结构 */
+ struct RegisterSigin {
+     char user_name[KUserNameMax];
+     char password[KUserPassMax];
+     MessageFlags flags;
+ };
+ /* 用户账户信息 */
+ struct User {
+   char user_name[KUserNameMax];
+   User(const char* s) {
+     strcpy(user_name, s);
+   }
+   User() {}
+ };
+ /* 群组结构 */
+ struct Group {
+   char group_name[KGroupName];
+   char admin_name[KGroupName];
+   std::vector<User> group_user;   //群成员
+ };
+ /* 用户所有信息 */
+ struct Users {
+   User user;      //用户账户
+   char password[KUserPassMax];
+   std::vector<User> friends;      // 好友
+   std::vector<Group> groups;      // 群组
+ };
+
+ /* 消息结构体 */
+ struct Message {
+   User sender;
+   User receiver;
+   char data[KDataMax];
+   char time[20];
+   MessageFlags flags;
+ };
+
+ void str_cli(Socket sockfd);
+ bool login(Socket sockfd);
+ void show_main_menu();
+ bool register_account(Socket sockfd);
+ Users my_user_info;
+
+ int main(int argc, char const *argv[]) {
+ #if defined(_WIN32_PLATFROM_)
+    WORD sock_version = MAKEWORD(2, 2);
+    WSADATA wsaData;
+    if (WSAStartup(sock_version, &wsaData) != 0) {
+      printf("wsa start error\n");
+      return 0;
+    }
+
+ #endif
+    Socket clientfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientfd < 0) {
+      return -1;
+    }
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(KServPort);
+    if (inet_pton(AF_INET, KServAddr, &servaddr.sin_addr) < 0) {
+      printf("inet_pton error\n");
+      return -1;
+    }
+
+    if (connect(clientfd, (sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+      printf("connect error\n");
+      return -1;
+    }
+
+    str_cli(clientfd);
+   return 0;
+ }
+
+ void str_cli(Socket sockfd)
+ {
+   while (!login(sockfd))
+    ;
+   show_main_menu();
+ }
+
+ bool login(Socket sockfd)
+ {
+   printf("             <----- 1. 登 录 ----->\n");
+   printf("             <----- 2. 注 册 ----->\n");
+   printf("             <----- 0. 注 册 ----->\n");
+   printf("     请输入: ");
+   int n;
+   scanf("%d", &n);
+   if (n == 1) {
+     std::string account;
+     std::string password;
+     printf("   账户: ");
+     std::cin >> account;
+     printf("   密码: ");
+     std::cin >> password;
+     if (account.size() > KUserNameMax || password.size() > KUserPassMax) {
+       std::cout << "   error : 账户或者密码太长 " << std::endl;
+       return false;
+     }
+     RegisterSigin mesg;
+     strcpy(mesg.user_name, account.c_str());
+     strcpy(mesg.password, password.c_str());
+     mesg.flags = SigIn;
+     if (send(sockfd, &mesg, strlen((char*)&mesg), 0) < 0) {
+       std::cout << "send error" << std::endl;
+       exit(-1);
+     }
+     Flags flags_mesg;
+     if (recv(sockfd, &flags_mesg, sizeof(flags_mesg), 0) < 0) {
+       std::cout << "recv error" << std::endl;
+       exit(-1);
+     }
+     if (flags_mesg.flags == Succees) {
+       if (recv(sockfd, &my_user_info, sizeof(Users), 0) < 0) {
+         std::cout << "recv error" << std::endl;
+         exit(-1);
+       }
+       return true;
+     }
+     else if (flags_mesg.flags == AccountError) {
+       std::cout << "账户或者密码错误" << std::endl;
+       return false;
+     } else {
+       std::cout << "账户已经登录" << std::endl;
+       return false;
+     }
+   }
+   else if (n == 2) {
+     register_account(sockfd);
+   }
+   else {
+     exit(0);
+   }
+   return false;
+ }
+
+bool register_account(Socket sockfd)
+{
+  return true;
+}
+
+ void show_main_menu()
+ {
+
+ }
--- server/server.cpp
+++ server/server.cpp
@@ -4,7 +4,7 @@
  * @Email:  guang334419520@126.com
  * @Filename: server.cpp
  * @Last modified by:   sunshine
- * @Last modified time: 2018-04-08T17:10:37+08:00
+ * @Last modified time: 2018-04-10T17:44:37+08:00
  */
 
 #include <iostream>
@@ -19,24 +19,25 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <vector>
+#include <string>
 #include <map>
 #include <fstream>
 
-const int KMaxLen = 4096;
-const char* KServHost = "127.0.0.1";
-const int KServPort = 9999;
-const int LISTENQ = 20;
-const int KUserNameMax = 12;
-const int KUserPassMax = 16;
-const int KGroupName = 12;
-const int KDataMax = 2048;
-const char* KUserInfoFileName = "userinfo";
+const int KMaxLen = 4096;           // 消息Max
+const int KServPort = 9999;         // 端口号
+const int LISTENQ = 20;             // listenq
+const int KUserNameMax = 12;        // max len of the username
+const int KUserPassMax = 16;        // max len of the password
+const int KGroupName = 12;          // max len of the group
+const int KDataMax = 2048;          // max len of the data
+const char* KUserInfoFileName = "userinfo";   // file name
 
-using Socket = int;
+using Socket = int;       // 方便使用
 
-pthread_mutex_t mutex;
+pthread_mutex_t mutex;      //互斥量，多线程所需
 
 
+/* 消息的类型 */
 enum MessageFlags {
   LeftBorder = 0,
   PrivateChat,
@@ -50,16 +51,23 @@ enum MessageFlags {
   RightBorder,
   SigIn = 64,
   Register,
-  AccountOrPassError = 128,
-  AccountError
+  Succees = 127,
+  AccountOrPassError,
+  AccountError,
+  AgainErro
+};
+// 用于 好友同意，拒绝，或者登录成功或者失败的消息结构体
+struct Flags {
+  MessageFlags flags;
 };
 
+/* 注册账号 结构 */
 struct RegisterSigin {
     char user_name[KUserNameMax];
     char password[KUserPassMax];
     MessageFlags flags;
 };
-
+/* 用户账户信息 */
 struct User {
   char user_name[KUserNameMax];
   User(const char* s) {
@@ -67,22 +75,26 @@ struct User {
   }
   User() {}
 };
-
+bool operator<(const User&x, const User& y) {
+    return strcmp(x.user_name, y.user_name) == -1;
+}
+/* 群组结构 */
 struct Group {
   char group_name[KGroupName];
   char admin_name[KGroupName];
-  std::vector<User> group_user;
+  std::vector<User> group_user;   //群成员
 };
-
+/* 用户所有信息 */
 struct Users {
-  User user;
+  User user;      //用户账户
   char password[KUserPassMax];
-  std::vector<User> friends;
-  std::vector<Group> groups;
+  std::vector<User> friends;      // 好友
+  std::vector<Group> groups;      // 群组
 };
 
-std::map<User, Users> users_info;
+std::map<User, Users> users_info;     //全局变量 用来保存所有用户的信息
 
+/* 消息结构体 */
 struct Message {
   User sender;
   User receiver;
@@ -91,24 +103,26 @@ struct Message {
   MessageFlags flags;
 };
 
+/* 客户端socket信息，和账户名 */
 struct Client {
   Socket fd;
   char name[KUserNameMax];
 }client[FD_SETSIZE];
 
-inline void error_deal(const char* s)
+inline void error_deal(const char* s)     // 错误处理函数
 {
   std::cerr << s << std::endl;
   exit(-1);
 }
 
-void private_chat(const Message& mesg);
-void friend_add(const Message& mesg);
-void friend_delete(const Message& mesg);
-void public_chat(const Message& mesg);
-void friend_agree_or_refused(const Message& mesg);
-void near_find(const Message& mesg);
+void private_chat(const Message& mesg);     // 私聊
+void friend_add(const Message& mesg);       // 添加好友
+void friend_delete(const Message& mesg);    // 删除好友
+void public_chat(const Message& mesg);      // 群聊
+void friend_agree_or_refused(const Message& mesg); //同意或者拒绝好友申请
+void near_find(const Message& mesg);        // 附近的人
 
+/* 函数指针数组 ， 用来保存各个消息类型所需的处理函数 */
 void (*option_fun[])(const Message& mesg) = {
   private_chat,
   friend_add,
@@ -130,6 +144,25 @@ int read_data(Socket sockfd, const char* name)
   }
 
   return n;
+}
+
+void friend_add(const Message& mesg)
+{
+
+}
+void friend_delete(const Message& mesg)
+{
+
+}
+
+void friend_agree_or_refused(const Message& mesg)
+{
+
+}
+
+void near_find(const Message& mesg)
+{
+  
 }
 
 void private_chat(const Message& mesg)
@@ -162,7 +195,7 @@ void public_chat(const Message& mesg)
 
   for (auto i : group_users) {
     for (auto j : client)
-      if (memcpy(i.user_name, j.name, KUserNameMax) == 0)
+      if (memcmp(i.user_name, j.name, KUserNameMax) == 0)
         if (send(j.fd, &mesg, strlen((char*)&mesg), 0) < 0)
           error_deal("public chat send error");
   }
@@ -238,15 +271,33 @@ void read_user_info_file()
     users_info.insert(std::make_pair(users.user, users));
   }
 }
-
-bool sign_in(const RegisterSigin& user_info)
+// return 0代表已经登录了，-1代表账户密码错误，1代表登录成功
+int sign_in(const RegisterSigin& user)
 {
+  for (auto i : client) {
+    if (strcmp(i.name, user.user_name) == 0)
+      return 0;
+  }
+
+  if (strcmp(users_info[User(user.user_name)].password, user.password) == 0)
+    return 1;
+  else
+    return -1;
 
 }
 
-bool register_account(const RegisterSigin& user_info)
+bool register_account(const RegisterSigin& user)
 {
 
+  if (users_info.find(User(user.user_name)) != users_info.end())
+    return false;
+
+  Users users;
+  memset(&users, 0, sizeof(users));
+  users.user = User(user.user_name);
+  memcpy(users.password, user.password, strlen(user.password));
+  users_info.insert(std::make_pair(users.user, users));
+  return true;
 }
 
 char* my_time()
@@ -260,6 +311,7 @@ void* client_thread(void* arg)
 {
   int i = *((int*)arg);
   RegisterSigin mesg;
+  Flags sig_mesg;
   while (true) {
     if (recv(client[i].fd, &mesg, sizeof(mesg), 0) < 0) {
       client[i].fd = -1;
@@ -268,25 +320,44 @@ void* client_thread(void* arg)
       pthread_exit(0);
     }
     pthread_mutex_lock(&mutex);
+    int n;
     if (mesg.flags == SigIn)
-      if (!sign_in(mesg)) {
-        mesg.flags = AccountOrPassError;
-        send(client[i].fd, (char*)&mesg, strlen((char*)&mesg), 0);
+      if ((n = sign_in(mesg)) < 0) {
+        sig_mesg.flags = AccountOrPassError;
+        send(client[i].fd, (char*)&sig_mesg, strlen((char*)&mesg), 0);
+        pthread_mutex_unlock(&mutex);
+        continue;
+      }
+      else if (n == 0){
+        sig_mesg.flags = AgainErro;
+        int len = strlen((char*)&mesg);
+        if (send(client[i].fd, (char*)&sig_mesg, len, 0) != len)
+          error_deal("error send");
         pthread_mutex_unlock(&mutex);
         continue;
       }
       else {
+        sig_mesg.flags = Succees;
+        int len = strlen((char*)&mesg);
+        if (send(client[i].fd, (char*)&sig_mesg, len, 0) != len)
+          error_deal("error send");
         pthread_mutex_unlock(&mutex);
         break;
       }
     else if (mesg.flags == Register) {
       if (!register_account(mesg)) {
-        mesg.flags = AccountError;
-        send(client[i].fd, (char*)&mesg, strlen((char*)&mesg), 0);
+        sig_mesg.flags = AccountError;
+        int len = strlen((char*)&mesg);
+        if (send(client[i].fd, (char*)&sig_mesg, len, 0) != len)
+          error_deal("error send");
         pthread_mutex_unlock(&mutex);
         continue;
       }
       else {
+        sig_mesg.flags = Succees;
+        int len = strlen((char*)&mesg);
+        if (send(client[i].fd, (char*)&sig_mesg, len, 0) != len)
+          error_deal("error send");
         pthread_mutex_unlock(&mutex);
         break;
       }
@@ -309,9 +380,11 @@ int main(int argc, char const *argv[]) {
   pthread_t thid;
 
   pthread_mutex_init(&mutex, NULL);
-  Socket listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  /* 创建 监听套接字 */
+  Socket listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (listenfd < 0)
     error_deal("socket error");
+  /* 初始化 sockaddr */
   struct sockaddr_in cliaddr, servaddr;
   bzero(&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
@@ -321,9 +394,10 @@ int main(int argc, char const *argv[]) {
   const int on = 1;
   if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
     error_deal("setsockopt reuseaddr error");
+  /* 地址绑定 */
   if (bind(listenfd, (sockaddr*)&servaddr, sizeof(servaddr)) < 0)
     error_deal("bind error");
-
+  /* 进行监听 */
   if (listen(listenfd, LISTENQ) < 0)
     error_deal("listen error");
 
@@ -346,6 +420,7 @@ int main(int argc, char const *argv[]) {
         error_deal("too many clients");
       time(&now);
       printf("有新的连接：ip:%s\t%s\n",inet_ntoa(cliaddr.sin_addr),ctime(&now));
+      /* 每一个客户都单独利用一个线程进行服务 */
       pthread_create(&thid, NULL, client_thread,(void*)&i);
 }
 
